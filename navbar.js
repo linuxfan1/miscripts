7837318583f96a4b0092757f440fa052bc6a837b
Nathan Metzger <linuxfan724@gmail.com>
2018-04-18 10:56:14 -0400

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -5,5 +5,7 @@ function navbarLoad() {
   } else if (document.getElementById('pageclass').innerHTML = "Other") {
     document.getElementById('tbcnavbar').innerHTML = "<a class='navbar-brand ' href='#'>Linuxfan1</a><ul class='navbar-nav'><li class='nav-item dropdown'><a class='nav-link dropdown-toggle' id='tbc-toggle' data-toggle='dropdown'>TBC</a><div class='dropdown-menu'><a class='dropdown-item' href='https://goo.gl/iMUzzr'>TBC</a><a class='dropdown-item' href='https://goo.gl/5N1Vpg'>2 Hour Delay</a><a class='dropdown-item'></a></div></li><li class='nav-item'><a class='nav-link' href='http://www.editey.com/preview/1oz7ztdAIrORCNRdFGNlvdcbbgbh-tZcU/chat.html?pe=nmetzger.2021@myiasd.cc'>Chat</a></li><button id='debugbutton' onclick='return debugFunctionEnable();' style='display:none;' class='nav-item btn btn-danger'>Debug</button></ul>";
     return pageLoad();
+  } else {
+    return False;
   }
 }
